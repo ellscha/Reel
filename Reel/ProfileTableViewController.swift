4eda61e2163b2f657700eb26f02f387379874c30
ellscha <escharlin@wesleyan.edu>
2017-12-02 11:48:57 -0500

    didn't do the log in, rather made the profile tableview controller

---
+++ Reel/ProfileTableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  ProfileTableViewController.swift
+//  Reel
+//
+//  Created by Elli Scharlin on 12/2/17.
+//  Copyright © 2017 YHackWes. All rights reserved.
+//
+
+import UIKit
+
+class ProfileTableViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 3
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        switch section {
+        case 0:
+            return 1
+        case 1:
+            return User.reel.count
+        case 2:
+            return User.queue.count
+        default:
+            return 1
+        }
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        switch indexPath.section {
+        case 0:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "profile", for: indexPath) as! ProfileTableViewCell
+            cell.nameLabel.text! = User.name
+            cell.profilePicView.image = UIImage.init(named: User.profilePicture)
+            cell.bioLabel.text! = User.bio
+            cell.handleLabel.text! = User.handle
+            return cell
+
+        case 1:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "reel", for: indexPath) as! ReelTableViewCell
+            cell.titleLabel.text = User.reel[indexPath.row]
+            return cell
+
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "queue", for: indexPath) as! QueueTableViewCell
+            cell.titleLabel.text = User.queue[indexPath.row]
+            return cell
+
+        }
+//        let profileCell = tableView.dequeueReusableCell(withIdentifier: "profile", for: indexPath)
+        
+        
+        // Configure the cell...
+    }
+    
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Reel/User.swift
@@ -0,0 +1,21 @@
+//
+//  User.swift
+//  Reel
+//
+//  Created by Elli Scharlin on 12/2/17.
+//  Copyright © 2017 YHackWes. All rights reserved.
+//
+
+import Foundation
+class User : NSObject {
+    static var name : String = "Rachel C. Wigmanstein"
+    static var handle : String = "@rcdubs"
+    static var bio : String = "I love comedy movies, and watching TV with my dog. I love comedy movies, and watching TV with my dog."
+    static var reel : [String] = ["0","1"]
+    static var queue : [String] = ["0", "1", "2","3"]
+
+    static var profilePicture : String = "profPic"
+    
+    
+    
+}
