1e33bc49c5eb205bd20264fae8dff600742b9cf4
ellscha <escharlin@wesleyan.edu>
2017-12-02 16:37:39 -0500

    working on datastore

---
+++ Reel/DataStore.swift
@@ -0,0 +1,12 @@
+//
+//  DataStore.swift
+//  
+//
+//  Created by Elli Scharlin on 12/2/17.
+//
+
+import Foundation
+class DataStore {
+    static let sharedInstance = DataStore()
+    private init(){}
+}
